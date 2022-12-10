261347ed7b7800428f2a6abfcc48bd13de7a4d68
engtheITspecialist <120220820+engtheITspecialist@users.noreply.github.com>
2022-12-10 23:49:19 +0300

    Create Messages.java

---
+++ Messages.java
@@ -0,0 +1,9 @@
+//written by engtheITSpecialist 
+//linkedin.com/in/engin-taş-8659537b
+
+//this class contain final static string messages.
+public class Messages {
+public static final String ADMIRE="you have known correctly, perfect";
+public static final String WRONGANSWER="Your answer is wrong \n";
+public static final String QUESTİON="What's result of ";
+}
